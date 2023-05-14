4a0ec15ba83a2fd0fd8b6404ffcd67add1f072dd
bjcarvalho007 <133596305+bjcarvalho007@users.noreply.github.com>
2023-05-14 15:41:34 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,25 @@
 const botaoAlterarTema  = document.getElementById("botao-alterar-tema");
- 
+
+ const body = document.querySelector("body");
 const body = document.querySelector("body");
 
-const imagembotaotrocadetema =document.querySelector(".imagem-botao")
+ const imagembotaotrocadetema =document.querySelector(".imagem-botao")
+ const imagembotaotrocadetema =document.querySelector(".imagem-botao")
 
 
 
-botaoAlterarTema.addEventListener("click", () =>{
-    
+ botaoAlterarTema.addEventListener("click", () =>{
+ botaoAlterarTema.addEventListener("click", () =>{
 
-    
+const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+body.classList.toggle("modo-escuro");
+
+if(modoEscuroEstaAtivo){
  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+  imagembotaotrocadetema.setAttribute("src","./src/imagens/sun.png");
 
- body.classList.toggle("modo-escuro");
-
-
- if(modoEscuroEstaAtivo){
-  
-
-imagembotaotrocadetema.setAttribute("src","./src/imagens/sun.png");
-
- } else {
-
-
-
-
-//- passo 5 - trocar a imagem do botão de alterar tema pra lua
-imagembotaotrocadetema.setAttribute("src","./src/imagens/moon.png");
-}
-
-
-});
-
+} else {
 
 
 
